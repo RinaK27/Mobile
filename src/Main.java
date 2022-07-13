3ee9065dd425f55049940636592b16a4c6c4fdb4
RinaK27 <megustarina@gmail.com>
2022-07-13 23:20:26 +0400

    "Приложение"

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int threshold = 1000; // порог пополнения для бонусов
+        int accountRefill; // пополнение счета
+        int amount = 100; // начальная сумма на счету
+        int bonus; // бонус за пополнение
+        accountRefill = 1100;
+        if(accountRefill > threshold) {
+            bonus = accountRefill / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println(bonus + " Бонусов за пополнение баланса");
+        System.out.println(amount + accountRefill + bonus + " Итоговый счёт");
+    }
+}
